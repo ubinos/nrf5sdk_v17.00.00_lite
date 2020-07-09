da4b8e4a0cd8de93e8d7724baaceee2aaec02dbe
Sungho Park <siminsungho@gmail.com>
2020-07-09 15:46:36 +0900

    modify library related to nrf52dk(pca10040)

--- components/boards/pca10040.h
+++ components/boards/pca10040.h
@@ -46,6 +46,81 @@ extern "C" {
 
 #include "nrf_gpio.h"
 
+#if defined (ENABLE_TRACE)
+
+// LEDs definitions for PCA10040
+#define LEDS_NUMBER    2
+
+#define LED_START      17
+#define LED_1          17
+#define LED_2          19
+#define LED_STOP       19
+
+#define LEDS_ACTIVE_STATE 0
+
+#define LEDS_INV_MASK  LEDS_MASK
+
+#define LEDS_LIST { LED_1, LED_2 }
+
+#define BSP_LED_0      LED_1
+#define BSP_LED_1      LED_2
+
+#define BUTTONS_NUMBER 1
+
+#define BUTTON_START   13
+#define BUTTON_1       13
+#define BUTTON_STOP    13
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+
+#define BUTTONS_ACTIVE_STATE 0
+
+#define BUTTONS_LIST { BUTTON_1 }
+
+#define BSP_BUTTON_0   BUTTON_1
+
+#else
+#if defined (ENABLE_SWO)
+
+// LEDs definitions for PCA10040
+#define LEDS_NUMBER    3
+
+#define LED_START      17
+#define LED_1          17
+#define LED_2          19
+#define LED_3          20
+#define LED_STOP       20
+
+#define LEDS_ACTIVE_STATE 0
+
+#define LEDS_INV_MASK  LEDS_MASK
+
+#define LEDS_LIST { LED_1, LED_2, LED_3 }
+
+#define BSP_LED_0      LED_1
+#define BSP_LED_1      LED_2
+#define BSP_LED_2      LED_3
+
+#define BUTTONS_NUMBER 4
+
+#define BUTTON_START   13
+#define BUTTON_1       13
+#define BUTTON_2       14
+#define BUTTON_3       15
+#define BUTTON_4       16
+#define BUTTON_STOP    16
+#define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
+
+#define BUTTONS_ACTIVE_STATE 0
+
+#define BUTTONS_LIST { BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4 }
+
+#define BSP_BUTTON_0   BUTTON_1
+#define BSP_BUTTON_1   BUTTON_2
+#define BSP_BUTTON_2   BUTTON_3
+#define BSP_BUTTON_3   BUTTON_4
+
+#else
+
 // LEDs definitions for PCA10040
 #define LEDS_NUMBER    4
 
@@ -85,6 +160,9 @@ extern "C" {
 #define BSP_BUTTON_1   BUTTON_2
 #define BSP_BUTTON_2   BUTTON_3
 #define BSP_BUTTON_3   BUTTON_4
+
+#endif /* defined (ENABLE_SWO) */
+#endif /* defined (ENABLE_TRACE) */
 
 #define RX_PIN_NUMBER  8
 #define TX_PIN_NUMBER  6
